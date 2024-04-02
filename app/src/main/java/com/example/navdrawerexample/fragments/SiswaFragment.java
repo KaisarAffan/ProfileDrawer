c9e2eb1f7c8f09d33dd22534badc63bbe29cbd2a
KasiarAffan <kaisaraffan1@gmail.com>
2024-04-02 08:05:33 +0700

    inital commit

---
+++ app/src/main/java/com/example/navdrawerexample/fragments/SiswaFragment.java
@@ -0,0 +1,50 @@
+package com.example.navdrawerexample.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.navdrawerexample.R;
+
+public class SiswaFragment extends Fragment {
+
+    private ListView listViewSiswa;
+
+    public SiswaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_siswa, container, false);
+
+        // Initialize ListView
+        listViewSiswa = rootView.findViewById(R.id.listViewStudents);
+
+        // Create an array of student names
+        String[] studentNames = {"Jahn Da", "Nich Gaer", "Loej ele", "Emily Johnson", "David Williams"};
+
+        // Create an ArrayAdapter to populate the ListView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, studentNames);
+
+        // Set the adapter to the ListView
+        listViewSiswa.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Set the title of the Fragment
+        requireActivity().setTitle("Guru Fragment");
+    }
+}
